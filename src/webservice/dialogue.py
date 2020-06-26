d39eb1a203c011e5a56ffb241503284686e7d458
Mark Snaith <msnaith@dundee.ac.uk>
2020-06-26 14:28:13 +0100

    Move .yml docs files into seperate folder

--- src/webservice/dialogue.py
+++ src/webservice/dialogue.py
@@ -10,40 +10,7 @@ class Dialogue:
     @ws.method("/new/<protocol>",methods=["POST"])
     def new(self, protocol):
         """
-        post:
-            summary: Create a new dialogue
-            security:
-                - APIAuthKey: []
-            responses:
-                '200':
-                    description: OK
-                    schema:
-                        type: object
-                        properties:
-                            foo:
-                                type: string
-                                example: bar
-            parameters:
-                - name: protocol
-                  in: path
-                  required: true
-                  description: The name of the protocol
-
-                - name: body
-                  in: body
-                  required: true
-                  schema:
-                      type: object
-                      properties:
-                          participants:
-                              type: array
-                              items:
-                                  type: object
-                                  properties:
-                                      name:
-                                          type: string
-                                      player:
-                                          type: string
+        @/app/docs/dialogue/new.yml
         """
 
         login = ws.authorise()
@@ -61,46 +28,7 @@ class Dialogue:
     @ws.method("/<dialogueID>/moves",methods=["GET"])
     def moves(self, dialogueID):
         """
-        get:
-            summary: Get the currently available moves for the given dialogueID
-            security:
-                - APIAuthKey: []
-            parameters:
-                - name: dialogueID
-                  in: path
-                  required: true
-                  description: The ID of the dialogue
-            responses:
-                '401':
-                    description: Not authorised
-                '404':
-                    description: Not found
-                '200':
-                    description: OK
-                    schema:
-                        type: object
-                        properties:
-                            dialogueID:
-                                type: string
-                            moves:
-                                type: object
-                                properties:
-                                    <player>:
-                                        type: array
-                                        items:
-                                            type: object
-                                            properties:
-                                                reply:
-                                                    type: object
-                                                    properties:
-                                                        p:
-                                                            type: string
-                                                        target:
-                                                            type: string
-                                                        moveID:
-                                                            type: string
-                                                        opener:
-                                                            type: string
+        @/app/docs/dialogue/moves.yml
         """
 
         login = ws.authorise()
@@ -121,40 +49,7 @@ class Dialogue:
     @ws.method("/<dialogueID>/interaction/<interactionID>",methods=["POST"])
     def interaction(self, dialogueID, interactionID):
         """
-        post:
-            summary: Performs the given interaction
-            security:
-                - APIAuthKey: []
-            responses:
-                '200':
-                    description: OK
-            parameters:
-                - name: dialogueID
-                  in: path
-                  required: true
-                  description: The ID of the dialogue in which to perform the interaction
-
-                - name: interactionID
-                  in: path
-                  required: true
-                  description: The ID of the interaction to perform
-
-                - name: body
-                  in: body
-                  required: true
-                  description: The body of the interaction
-                  schema:
-                    type: object
-                    properties:
-                        speaker:
-                            type: string
-                        target:
-                            type: string
-                        reply:
-                            type: object
-                            properties:
-                                p:
-                                    type: string
+        @/app/docs/dialogue/interaction.yml
         """
 
         login = ws.authorise()
