ba8a898d65ce852270d5c99fc13b735a0407b001
Mark Snaith <msnaith@dundee.ac.uk>
2020-07-02 16:30:28 +0100

    Finalise protocol ws methods

--- src/webservice/protocol.py
+++ src/webservice/protocol.py
@@ -7,9 +7,7 @@ import json
 
 _protocol_player_regex = re.compile(r"player\(([^\(\)]+)\)")
 _protocol_player_spec_regex = re.compile(r"([^:{}, ]+):(?:([^:,{} ]+)|{([^{}]+)})")
-
-
-_protocol_player_roles_regex = re.compile(r"{([^{}]+)}")
+_protocol_description_regex = re.compile(r"\/\*[\s]+description:[\s]+(.*)\*\/",re.IGNORECASE)
 
 @ws.group
 class Protocol:
@@ -21,11 +19,7 @@ class Protocol:
         """
         @/app/docs/protocol/get.yml
         """
-        mongo = pymongo.MongoClient("mongodb://dgep_mongo:27017/")
-        db = mongo["dgep"]
-        protocols = db["protocols"]
-
-        result = protocols.find_one({"name": protocol})
+        result = self.get_protocol(protocol)
 
         if result is not None:
             dgdl = result["dgdl"]
@@ -47,9 +41,10 @@ class Protocol:
         to_return = {}
 
         for protocol in result:
-            p = {"players": self.get_players(str(protocol["dgdl"]))}
-
-            to_return[protocol["name"]] = p
+            to_return[protocol["name"]] = {
+                    "players": self.get_players(protocol["dgdl"].decode("utf-8")),
+                    "description": self.get_description(protocol["dgdl"].decode("utf-8"))
+                }
 
         return to_return, 200
 
@@ -58,22 +53,17 @@ class Protocol:
         """
         @/app/docs/protocol/new.yml
         """
-
         file = ws.request.files['dgdl_file']
+        name = secure_filename(file.filename)
+        name = name.split(".")[0]
+        content = file.read()
 
         mongo = pymongo.MongoClient("mongodb://dgep_mongo:27017/")
         db = mongo["dgep"]
         protocols = db["protocols"]
 
-        name = secure_filename(file.filename)
-        name = name.split(".")[0]
-
-        content = file.read()
-
-        query = {"name": name}
-
-        if protocols.find_one(query) is not None:
-            protocols.update_one(query, {"$set": {"dgdl": content}})
+        if self.get_protocol(name) is not None:
+            protocols.update_one({"name":name}, {"$set": {"dgdl": content}})
         else:
             protocols.insert_one({"name": name, "dgdl": content})
 
@@ -88,47 +78,83 @@ class Protocol:
         file = ws.request.files['dgdl_file']
 
         if file:
-            d = file.read().decode("utf-8")
-            print(d)
-            parser = dgdl.DGDLParser()
-            game = parser.parse(input=d)
-
-            if isinstance(game, list):
-                return {"status":"error","errors": game}, 200
-            else:
-                return {"status":"OK"}, 200
+            return self.test_protocol(file=file), 200
         else:
-            return "No file read", 500
-
-
+            return "Error testing protocol", 500
 
     @ws.method("/test/<protocol>")
     def test(self, protocol):
         """
         @/app/docs/protocol/test.yml
         """
+        return self.test_protocol(protocol=protocol)
 
-        return protocol
 
+    def test_protocol(self, file=None, protocol=None):
+        """
+        Test the given DGDL file (prioritised), or protocol
+        """
 
+        if file is not None:
+            d = file.read().decode("utf-8")
+        elif protocol is not None:
+            d = self.get_protocol(protocol)
+            if d is None:
+                return "Protocol not found", 404
+        else:
+            return "Error testing protocol", 500
+
+        game = dgdl.DGDLParser().parse(input=d)
+
+        if isinstance(game, list):
+            return {"status":"error","errors":game}
+        else:
+            return {"status":"OK"}
+
+    def get_protocol(self, protocol):
+        """
+        Get the given protocol from mongodb
+        """
+        mongo = pymongo.MongoClient("mongodb://dgep_mongo:27017/")
+        db = mongo["dgep"]
+        protocols = db["protocols"]
+
+        result = protocols.find_one({"name": protocol})
+
+        if result is not None:
+            return result["dgdl"].decode("utf-8")
+        else:
+            return None
 
     def get_players(self, dgdl):
+        """
+        Extract the player specifications from the given dgdl
+        """
         matches = re.findall(_protocol_player_regex, dgdl)
         players = []
 
         for m in matches:
             if m.strip() != "":
                 player = {}
-                print(m)
                 for p in re.findall(_protocol_player_spec_regex, m.strip()):
-                    p = [x for x in p if x.strip() is not ""]
+                    p = [x for x in p if x.strip() != ""]
                     if p[0] == "roles":
                         player[p[0]] = [r.strip() for r in p[1].split(",")]
                     else:
                         player[p[0]] = p[1]
-
-
-
                 players.append(player)
 
         return players
+
+    def get_description(self, dgdl):
+        """
+        Extract the content of the 'description' keyword from the DGDL spec
+        """
+        matches = re.findall(_protocol_description_regex, dgdl)
+
+        for m in matches:
+            m = m.strip()
+            if m != "":
+                return m
+
+        return ""
