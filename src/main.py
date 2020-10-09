b145b3f5101777e6d22bc38d2170b47cede8ba5a
Mark Snaith <msnaith@dundee.ac.uk>
2020-10-09 13:39:06 +0100

    Fix merge conflicts

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from dgep import *
 
 if __name__ == '__main__':
@@ -25,3 +26,30 @@ if __name__ == '__main__':
 
 
     print(d.stores)
+=======
+import dgep
+from dialogue_manager import DialogueManager
+from interfaces.rest import app
+from flask import Flask
+
+class Test(Flask):
+
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+
+    def route(self, *args, **kwargs):
+        print("Hello, world", flush=True)
+        return super().route(*args, **kwargs)
+
+
+testapp = Test(__name__)
+
+@testapp.route("/dgep/new")
+def new():
+    return "Hello world"
+
+if __name__ == "__main__":
+    print("Running DGEP")
+    #dgep.run(DialogueManager)
+    app.run()
+>>>>>>> Stashed changes
